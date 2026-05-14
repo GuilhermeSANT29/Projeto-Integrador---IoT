86eadd50ee449f1df151bdc24edc18c4829bd0f9
Guilherme Santos <contate.guilherme.santos@gmail.com>
2026-05-14 11:10:56 -0300

    Delete 03_Modelagem_Sistema directory

--- 03_Modelagem_Sistema/Automação_DB projetc/querys/info.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO sensores (nome, tipo, unidade_medida) VALUES 
-('DHT11_Temp', 'Temperatura', '°C'),
-('DHT11_Umi', 'Umidade', '%'),
-('Sensor_Rotacao', 'Frequência', 'RPM');
--- 03_Modelagem_Sistema/Automação_DB projetc/security/user e senha.sql
+++
@@ -1,2 +0,0 @@
-CREATE USER IF NOT EXISTS 'user_wemos16'@'localhost'
-IDENTIFIED BY 'SenhaForte123!';
