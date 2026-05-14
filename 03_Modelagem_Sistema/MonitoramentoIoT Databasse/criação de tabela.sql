5eb6cb9e9682f8daf16631febdae62661f12d07b
Guilherme Santos <contate.guilherme.santos@gmail.com>
2026-05-14 14:38:59 -0300

    Delete 03_Modelagem_Sistema/MonitoramentoIoT Databasse directory

--- 03_Modelagem_Sistema/MonitoramentoIoT Databasse/criação de tabela.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE sensors (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    temp DECIMAL(5,2),
-    hum DECIMAL(5,2),
-    rpm INT,
-    ir INT,
-    status VARCHAR(20),
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-);
